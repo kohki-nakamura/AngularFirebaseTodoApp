2cc1c9d41630ed59defccf7d5d610ce156d4c7fb
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2021-02-15 00:28:06 +0900

    add: Firebase連携・デプロイ設定追加

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCGPJXlJ5KCTPIgs6NYQIbqceb04yBu8-U',
+    authDomain: 'angularfirebasetodoapp.firebaseapp.com',
+    projectId: 'angularfirebasetodoapp',
+    storageBucket: 'angularfirebasetodoapp.appspot.com',
+    messagingSenderId: '403521488647',
+    appId: '1:403521488647:web:56e49cf3e95f77e15f67a2',
+    measurementId: 'G-X1NEMBNVGG'
+  }
 };
