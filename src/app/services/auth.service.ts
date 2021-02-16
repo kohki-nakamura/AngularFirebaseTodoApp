75587ebb1323deae5c5ad0eb9836c0ee0e7fa79d
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2021-02-16 11:20:50 +0900

    add: googleアカウントでのログイン機能を追加（認証設定が分からず、ポップアップが機能しない）

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from 'firebase/app';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  user$ = this.afAuth.user;
+  constructor(private afAuth: AngularFireAuth) {}
+  login() {
+    this.afAuth
+    .signInWithPopup(new firebase.auth.GoogleAuthProvider())
+    .then((result) => {
+      console.log(result);
+    });
+  }
+  logout() {
+    this.afAuth.signOut();
+  }
+}
