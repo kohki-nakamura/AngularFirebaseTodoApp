6c7ec909f1ac6567e2f37d267552741d11fda719
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2021-02-16 14:33:20 +0900

    fix: ログイン処理を公式ドキュメントに沿って書き直し

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,17 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import firebase from 'firebase/app';
+
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase/app";
+// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
+// import * as firebase from "firebase/app"
+
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import "firebase/analytics";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
 
 @Injectable({
   providedIn: 'root',
@@ -9,11 +20,16 @@ export class AuthService {
   user$ = this.afAuth.user;
   constructor(private afAuth: AngularFireAuth) {}
   login() {
-    this.afAuth
-    .signInWithPopup(new firebase.auth.GoogleAuthProvider())
-    .then((result) => {
-      console.log(result);
-    });
+    var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+    
+    firebase.auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        console.log(result);
+      }).catch((error) => {
+        console.log(error);
+      });
   }
   logout() {
     this.afAuth.signOut();
