99ac9ba5a7e1cd26ea21ea8b70c2a63694d2a30b
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2021-02-16 12:08:14 +0900

    add: Todo作成用フォームを追加

--- src/app/todo/todo/todo.component.ts
+++ src/app/todo/todo/todo.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { tap } from 'rxjs/operators';
+import { TodoService } from 'src/app/services/todo.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.scss']
+  styleUrls: ['./todo.component.scss'],
 })
 export class TodoComponent implements OnInit {
+  todos$ = this.todo.getTodos().pipe(tap((data) => console.log(data)));
+  user$ = this.auth.user$;
+  lists = ['todo', 'doing', 'done'];
+  form = this.fb.group({
+    name: ['', [Validators.required, Validators.maxLength(10)]],
+  });
+  constructor(
+    private todo: TodoService,
+    private auth: AuthService,
+    private fb: FormBuilder
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  createTodo(uid: string) {
+    const name = this.form.controls.name.value;
+    this.todo.createTodo(name, uid);
+    this.form.reset();
+    this.form.controls.name.setErrors({ required: null });
   }
 
 }
