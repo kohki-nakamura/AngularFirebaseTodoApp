d558981c1973d22ba3d3bbceaa7d671bf6e7a6bf
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2021-02-16 14:50:33 +0900

    fix: login

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,17 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-
-// Firebase App (the core Firebase SDK) is always required and must be listed first
-import firebase from "firebase/app";
-// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
-// import * as firebase from "firebase/app"
-
-// If you enabled Analytics in your project, add the Firebase SDK for Analytics
-import "firebase/analytics";
-
-// Add the Firebase products that you want to use
-import "firebase/auth";
-import "firebase/firestore";
+import firebase from 'firebase/app';
 
 @Injectable({
   providedIn: 'root',
@@ -20,16 +9,14 @@ export class AuthService {
   user$ = this.afAuth.user;
   constructor(private afAuth: AngularFireAuth) {}
   login() {
-    var provider = new firebase.auth.GoogleAuthProvider();
-    provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-    
-    firebase.auth()
-      .signInWithPopup(provider)
-      .then((result) => {
-        console.log(result);
-      }).catch((error) => {
-        console.log(error);
-      });
+    this.afAuth
+    .signInWithPopup(new firebase.auth.GoogleAuthProvider())
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
   }
   logout() {
     this.afAuth.signOut();
